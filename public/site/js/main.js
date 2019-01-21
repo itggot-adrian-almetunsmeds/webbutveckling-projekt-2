b947b373cdee68426e134411cb3cf0e19a14d8fb
itggot-adrian-almetunsmeds <adrian.almetunsmeds@elev.ga.ntig.se>
2019-01-21 20:02:28 +0100

    Code styling

--- public/site/js/main.js
+++ public/site/js/main.js
@@ -11,7 +11,7 @@ let navBackground = document.querySelector(".nav_background");
 navBackground.addEventListener("click", navigation)
 let navButton2 = document.querySelector(".nav_menu > i ");
 navButton2.addEventListener("click", navigation)
-
+// Function for the nav menu
 function navigation(input) {
     navMenu = document.querySelector(".nav_menu");
     navMenu.classList.toggle("nav_active");
@@ -23,6 +23,71 @@ function navigation(input) {
         navIcon.innerHTML = "menu";
     } else {
         navIcon.innerHTML = "close";
+    }
+}
+// Function for the calendar popups
+function calendarReveal(input) {
+    if (input != "close") {
+        let id = "calendar_content_" + input
+        document.getElementById(id).classList.toggle("calendar_content_active");
+        calendarBackground.classList.toggle("calendar_background_active");
+        lastActive = document.getElementById(id);
+    }
+    if (input === "close") {
+        lastActive.classList.toggle("calendar_content_active");
+        calendarBackground.classList.toggle("calendar_background_active");
+    }
+}
+// Dropdown menu function
+function dropDownMenu(input) {
+    let dropDownMenu = document.querySelector(".deals_dropdown_menu");
+    dropDownMenu.classList.toggle("show_menu");
+}
+// Countdown timer
+function Updatetime(input) {
+    let today = new Date().getTime();
+    let deltaTime = countDownTarget - today;
+    let days = Math.floor(deltaTime / (1000 * 60 * 60 * 24));
+    let hours = Math.floor(deltaTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    let minutes = Math.floor(deltaTime % (1000 * 60 * 60) / (1000 * 60));
+    let secounds = Math.floor(deltaTime % (1000 * 60) / (1000));
+    document.querySelector(".time_left > p").innerHTML = days + " : " + hours + " : " + minutes + " : " + secounds + " ";
+
+    if (deltaTime < 0 && deltaTime > (-86400000)) {
+        christmasDay()
+    } else if (deltaTime < (-86400000)) {
+        error()
+    }
+}
+
+// Used in conjunction with the countdown
+function error(input) {
+    let placeError = document.querySelector(".countdown")
+    placeError.innerHTML = "An error has occurred. Please report this to the administrator."
+}
+
+function christmasDay(input) {
+    let placeError = document.querySelector(".countdown")
+    placeError.innerHTML = "Det är julafton idag!"
+}
+// Used to create calendar doors
+function doors(day) {
+    const adventCalendar = document.querySelector(".calendar")
+    var node = document.createElement("li");
+    node.id = "door" + day;
+    node.className = "door";
+    adventCalendar.appendChild(node);
+
+    var innerNode = document.createElement("a");
+    document.getElementById("door" + day).appendChild(innerNode);
+    innerNode.innerHTML = day
+
+    // If the date has not passed then add the disabled class to it.
+    let currentDate = new Date();
+    if ((currentDate.getMonth() + 1) < targetmonth || currentDate.getDate() < day) {
+        node.className += " disabled"
+    } else {
+        node.setAttribute('onclick', `calendarReveal(${day})`);
     }
 }
 
@@ -37,92 +102,28 @@ if (window.location.href.includes("deals") === true) {
     // Current position highlighted in nav
     let navActive = document.getElementById("dagens_deals");
     navActive.classList.toggle("nav_current");
-
-    function dropDownMenu(input) {
-        let dropDownMenu = document.querySelector(".deals_dropdown_menu");
-        dropDownMenu.classList.toggle("show_menu");
-    }
 }
-
 // JS functions to load on the index page
 if (window.location.href.includes("index.html") === true) {
     // Current position highlighted in nav
     let navActive = document.getElementById("hem");
     navActive.classList.toggle("nav_current");
-
     // THIS IS FOR THE COUNTDOWN TIMER
     Updatetime();
-    // Call the function when loaded to avoid the 1000ms initial gap
+    // Call the function when loaded to avoid the 1000ms initial gap then setInterval
     var countDownTarget = new Date(targetdate).getTime();
     var updateInterval = setInterval(Updatetime, 1000);
-
-    function Updatetime(input) {
-        let today = new Date().getTime();
-        let deltaTime = countDownTarget - today;
-        let days = Math.floor(deltaTime / (1000 * 60 * 60 * 24));
-        let hours = Math.floor(deltaTime % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
-        let minutes = Math.floor(deltaTime % (1000 * 60 * 60) / (1000 * 60));
-        let secounds = Math.floor(deltaTime % (1000 * 60) / (1000));
-        document.querySelector(".time_left > p").innerHTML = days + " : " + hours + " : " + minutes + " : " + secounds + " ";
-
-        if (deltaTime < 0 && deltaTime > (-86400000)) {
-            christmasDay()
-        } else if (deltaTime < (-86400000)) {
-            error()
-        }
-    }
-
-    function error(input) {
-        let placeError = document.querySelector(".countdown")
-        placeError.innerHTML = "An error has occurred. Please report this to the administrator."
-    }
-
-    function christmasDay(input) {
-        let placeError = document.querySelector(".countdown")
-        placeError.innerHTML = "Det är julafton idag!"
-    }
 
     // This is for the calendar
     // It has NO autoupdate at the moment. TODO: Add auto update of doors and not 
     // only on page load
-
-    // Create 24 li items
+    // Create 24 li items/doors
     for (i = 1; i <= 24; i++) {
         doors(i)
     }
-
-    function doors(day) {
-        const adventCalendar = document.querySelector(".calendar")
-        var node = document.createElement("li");
-        node.id = "door" + day;
-        node.className = "door";
-        adventCalendar.appendChild(node);
-
-        var innerNode = document.createElement("a");
-        document.getElementById("door" + day).appendChild(innerNode);
-        innerNode.innerHTML = day
-
-        // If the date has not passed then add the disabled class to it.
-        let currentDate = new Date();
-        if ((currentDate.getMonth() + 1) < targetmonth || currentDate.getDate() < day) {
-            node.className += " disabled"
-        } else {
-            node.setAttribute('onclick', `calendarReveal(${day})`);
-        }
-    }
-    // Function for the calendar popups
-    function calendarReveal(input) {
-        if (input != "close") {
-            let id = "calendar_content_" + input
-            document.getElementById(id).classList.toggle("calendar_content_active");
-            calendarBackground.classList.toggle("calendar_background_active");
-            lastActive = document.getElementById(id);
-        }
-        if (input === "close") {
-            lastActive.classList.toggle("calendar_content_active");
-            calendarBackground.classList.toggle("calendar_background_active");
-        }
-    }
 }
+
+
+
 
 // console.log("test")
